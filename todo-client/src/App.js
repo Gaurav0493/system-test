90cd668743500be851d4fb9d1e5362a397ab023d
Gaurav0493 <gauravkr93.gk@gmail.com>
2020-02-19 22:29:40 +0530

    Initial commmit

--- todo-client/src/App.js
+++ todo-client/src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EnhancedTable from './components/Table/TableList';
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import { orange } from '@material-ui/core/colors';
+import CustomAppBar from './components/AppBar'
+
+const theme = createMuiTheme({
+  status: {
+    danger: orange[500],
+  },
+});
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <CustomAppBar/>
+    <EnhancedTable/>
     </div>
   );
 }
